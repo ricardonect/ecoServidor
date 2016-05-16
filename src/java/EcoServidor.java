b3c0dd0786f21761825b38337402b2f137cad8c0
ricardonect <ricardonect@gmail.com>
2016-05-16 11:05:26 -0500

    Inicio del proyecto

---
+++ src/java/EcoServidor.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ricardo Marín
+ */
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EcoServidor implements Runnable {
+
+    public static final int PORT = 1134;
+
+    public void procedimiento() throws IOException {
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Establece el puerto en el que escucha peticiones
+            ServerSocket socketServidor = null;
+            try {
+                socketServidor = new ServerSocket(PORT);
+            } catch (IOException e) {
+                System.out.println("No puede escuchar en el puerto: " + PORT);
+                System.exit(-1);
+            }
+            
+            int cont = 0;
+            
+            Socket socketCliente = null;
+            BufferedReader entrada = null;
+            PrintWriter salida = null;
+            
+            System.out.println("Escuchando: " + socketServidor);
+            
+            try {
+                // Se bloquea hasta que recibe alguna petición de un cliente
+                // abriendo un socket para el cliente
+                socketCliente = socketServidor.accept();
+                System.out.println("Connexión acceptada: " + socketCliente);
+                // Establece canal de entrada
+                entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                // Establece canal de salida
+                salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
+                
+                // Hace eco de lo que le proporciona el cliente, hasta que recibe "Adios"
+                while (true) {
+                    cont = cont + 5;
+                    String str = entrada.readLine();
+                    System.out.println("Cliente: " + str);
+                    salida.println(cont);
+                    if (cont == 60) {
+                        cont = 0;
+                    }
+                    
+                    if (str.equals("Adios")) {
+                        break;
+                    }
+                }
+                
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+            salida.close();
+            entrada.close();
+            socketCliente.close();
+            socketServidor.close();
+        } catch (IOException ex) {
+            Logger.getLogger(EcoServidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ web/Home.jsp
@@ -0,0 +1,51 @@
+<%-- 
+    Document   : Home
+    Created on : 29/04/2016, 02:00:48 PM
+    Author     : RicardoMarin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>..::EcoServior::..</title>
+
+    </head>
+    <body>
+       <div class="wrapper">
+	<div class="container">
+            <div><br/></div>
+            <div><br/></div>
+            <div><br/></div>
+            <div><br/></div>
+            <div><br/></div>
+            <div><br/></div>
+            <div><br/></div>
+      <h1>ECO SERVIDOR</h1>
+        <form action="Eco" method="GET">
+            <input type="submit" id="btnIniciar" value="Iniciar"/>
+        </form>
+      
+	</div>
+
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+        <script src="js/jquery.min.js"></script>
+
+    </body>
+    
+</html>
